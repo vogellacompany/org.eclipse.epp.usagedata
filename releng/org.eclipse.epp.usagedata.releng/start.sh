1c66433ef5d220bd0507087cc268e0c956becfee
Markus Knauer <mknauer@eclipsesource.com>
2008-01-24 20:40:46 +0000

    create simple site.xml

--- releng/org.eclipse.epp.usagedata.releng/start.sh
+++ releng/org.eclipse.epp.usagedata.releng/start.sh
@@ -3,6 +3,10 @@
 JAVA=/opt/ibm/java2-ppc-50/bin/java
 BUILD_ROOT=/shared/technology/epp/udc_build
 
+BUILD_DATE=`date +%Y%m%d`
+BUILD_TIME=`date +%H%M`
+TIMESTAMP=${BUILD_DATE}${BUILD_TIME}
+
 rm -r ${BUILD_ROOT}/workspace
 
 ${JAVA} -jar ${BUILD_ROOT}/eclipse/plugins/org.eclipse.equinox.launcher_1.0.100.v20071211.jar \
@@ -10,4 +14,5 @@ ${JAVA} -jar ${BUILD_ROOT}/eclipse/plugins/org.eclipse.equinox.launcher_1.0.100.
         -buildfile ${BUILD_ROOT}/eclipse/plugins/org.eclipse.pde.build_3.4.0.v20071212/scripts/build.xml \
         -Dbuilder=${BUILD_ROOT}/org.eclipse.epp/releng/org.eclipse.epp.usagedata.releng/ \
         -DbuildDirectory=${BUILD_ROOT}/workspace/ \
-        -Dbase=${BUILD_ROOT}
+        -Dbase=${BUILD_ROOT} \
+        -DbuildId=${TIMESTAMP}
