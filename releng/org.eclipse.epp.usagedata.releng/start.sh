7f90c116b1d207233f98cdce3a61f87f8be0ca31
Markus Knauer <mknauer@eclipsesource.com>
2009-05-04 18:28:52 +0000

    1.1.0 releng update

--- releng/org.eclipse.epp.usagedata.releng/start.sh
+++ releng/org.eclipse.epp.usagedata.releng/start.sh
@@ -34,7 +34,7 @@ fi
 # Take build type from the command-line; assume 'N' if not specified.
 BUILD_TYPE=${1:-N} 
 
-ECLIPSE_ROOT=${ECLIPSE_BASES}/eclipse-SDK-3.5M6-linux-gtk/eclipse
+ECLIPSE_ROOT=${ECLIPSE_BASES}/eclipse-SDK-3.5M7-linux-gtk/eclipse
 UPDATE_SITE=${UPDATE_ROOT}/${BUILD_TYPE}
 
 BUILD_DATE=`date +%Y%m%d`
