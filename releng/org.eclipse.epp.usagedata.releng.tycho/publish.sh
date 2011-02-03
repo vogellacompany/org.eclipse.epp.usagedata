9b8d3c4f4a9fcd2bba669e95b86807dc9b999831
Wayne Beaton <wayne@eclipse.org>
2011-02-03 17:22:52 +0000

    *** empty log message ***

--- releng/org.eclipse.epp.usagedata.releng.tycho/publish.sh
+++ releng/org.eclipse.epp.usagedata.releng.tycho/publish.sh
@@ -25,13 +25,13 @@ do
   	esac
 done
 
-target=org.eclipse.epp/releng/org.eclipse.epp.usagedata.repository/target/site
+target=`pwd`/org.eclipse.epp/releng/org.eclipse.epp.usagedata.repository/target
 launcher=`find ${eclipse} -type f -name 'org.eclipse.equinox.launcher_*.jar' -print0`
 java -jar ${launcher} \
  -application org.eclipse.equinox.p2.publisher.UpdateSitePublisher \
- -metadataRepository ${target}/meta \
- -artifactRepository ${target}/art \
- -source org.eclipse.epp/releng/org.eclipse.epp.usagedata.repository/target/site \
+ -metadataRepository ${target}/publish \
+ -artifactRepository ${target}/publish \
+ -source ${target}/site \
  -compress \
  -publishArtifacts
  
