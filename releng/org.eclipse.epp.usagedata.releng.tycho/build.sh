9bbb1273d3d1bf7b54128f382c2a92eabc69c35d
Wayne Beaton <wayne@eclipse.org>
2011-02-03 15:52:47 +0000

    *** empty log message ***

--- releng/org.eclipse.epp.usagedata.releng.tycho/build.sh
+++ releng/org.eclipse.epp.usagedata.releng.tycho/build.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
+#/*******************************************************************************
+# * Copyright (c) 2011 Eclipse Foundation and others.
+# * All rights reserved. This program and the accompanying materials
+# * are made available under the terms of the Eclipse Public License v1.0
+# * which accompanies this distribution, and is available at
+# * http://www.eclipse.org/legal/epl-v10.html
+# *
+# * Contributors:
+# *    Wayne Beaton (Eclipse Foundation) - Initial implementation
+# *******************************************************************************/
+#
 root=:pserver:anonymous@dev.eclipse.org:/cvsroot/technology
 path=org.eclipse.epp
+releng=$path/releng/org.eclipse.epp.usagedata.releng.tycho
+repository=$path/releng/org.eclipse.epp.usagedata.repository
 target=workspace
-tag=${1:-HEAD}
-echo $tag
+#defaults
+tag=HEAD
+eclipse=~/Eclipse/eclipse-rcp-helios-SR1-linux-gtk/eclipse/
+
+for arg in $*
+do
+	case $arg in
+    	--tag=*)
+		tag=`echo $arg | sed 's/[-a-zA-Z0-9]*=//'`
+		;;
+    	--eclipse=*)
+		user=`echo $arg | sed 's/[-a-zA-Z0-9]*=//'`
+		;;
+		--help)
+		echo -e "./build.sh [--tag={tag}} [--eclipse={path}]\nWhere {tag} is the CVS tag to build (default: HEAD) and {path} points to an Eclipse install (for Pack200)"
+		exit
+		;;
+  	esac
+done
+
+echo "Extracting ${tag} from CVS..."
 cvs -d $root checkout -r $tag $path/features/org.eclipse.epp.usagedata.feature
 cvs -d $root checkout -r $tag $path/plugins/org.eclipse.epp.usagedata.gathering
 cvs -d $root checkout -r $tag $path/plugins/org.eclipse.epp.usagedata.recording
@@ -14,6 +46,12 @@ cvs -d $root checkout -r $tag $path/test/org.eclipse.epp.usagedata.recording.tes
 cvs -d $root checkout -r $tag $path/test/org.eclipse.epp.usagedata.ui.tests
 cvs -d $root checkout -r $tag $path/releng/org.eclipse.epp.usagedata.repository
 cvs -d $root checkout -r $tag $path/releng/org.eclipse.epp.usagedata.releng.tycho
-cd $path/releng/org.eclipse.epp.usagedata.releng.tycho
-mvn clean install
 
+echo "Starting Maven build..."
+mvn -B --file ${releng}/pom.xml clean install
+
+echo "Starting Pack200"
+jarprocessor=`find ${eclipse} -type f -name 'org.eclipse.equinox.p2.jarprocessor_*.jar' -print0`
+echo "- Found JAR Processor at ${jarprocessor}"
+java -jar ${jarprocessor} -verbose -outputDir ${repository}/target/pack200/ -processAll -repack -pack ${repository}/target/site_assembly.zip 
+    
