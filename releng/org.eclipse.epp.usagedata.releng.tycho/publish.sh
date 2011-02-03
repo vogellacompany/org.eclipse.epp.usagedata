1fa036f7a27dcfe54de799014c373cf25507b6d5
Wayne Beaton <wayne@eclipse.org>
2011-02-03 17:15:28 +0000

    *** empty log message ***

---
+++ releng/org.eclipse.epp.usagedata.releng.tycho/publish.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#/*******************************************************************************
+# * Copyright (c) 2011 Eclipse Foundation and others.
+# * All rights reserved. This program and the accompanying materials
+# * are made available under the terms of the Eclipse Public License v1.0
+# * which accompanies this distribution, and is available at
+# * http://www.eclipse.org/legal/epl-v10.html
+# *
+# * Contributors:
+# *    Wayne Beaton (Eclipse Foundation) - Initial implementation
+# *******************************************************************************/
+#
+eclipse=~/Eclipse/eclipse-rcp-helios-SR1-linux-gtk/eclipse/
+
+for arg in $*
+do
+	case $arg in
+    	--eclipse=*)
+		user=`echo $arg | sed 's/[-a-zA-Z0-9]*=//'`
+		;;
+		--help)
+		echo -e "./publish.sh [--eclipse={path}]\n Where {path} points to an Eclipse install (default ~/Eclipse/eclipse-rcp-helios-SR1-linux-gtk/eclipse/)"
+		exit
+		;;
+  	esac
+done
+
+target=org.eclipse.epp/releng/org.eclipse.epp.usagedata.repository/target/site
+launcher=`find ${eclipse} -type f -name 'org.eclipse.equinox.launcher_*.jar' -print0`
+java -jar ${launcher}
+ -application org.eclipse.equinox.p2.publisher.UpdateSitePublisher
+ -metadataRepository ${target}
+ -artifactRepository ${target}
+ -source org.eclipse.epp/releng/org.eclipse.epp.usagedata.repository/target/site
+ -compress 
+ -publishArtifacts
